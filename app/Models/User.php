c1065d710621d23b4f570b098598b440845cfdea
joerenzcarlmiranda <mirandajoerenz23@gmail.com>
2026-06-22 18:53:27 +0800

    feat: #13 Add Patient model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function patient()
+    {
+        return $this->hasOne(Patient::class);
+    }
 }
---
+++ database/migrations/2026_06_22_102539_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')
+            ->nullable()
+            ->constrained()
+            ->nullOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('address');
+            $table->string('contact_number');
+            $table->string('emergency_contact_name');
+            $table->string('emergency_contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
