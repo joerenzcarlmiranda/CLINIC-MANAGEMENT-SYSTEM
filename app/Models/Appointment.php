4761c246c64cad3f2c0ea8c765c1328ce3a99255
joerenzcarlmiranda <mirandajoerenz23@email.com>
2026-06-27 22:08:55 +0800

    feat: #29 Add Appointment Model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\AppointmentStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function casts(): array
+    {
+        return [
+            'appointment_date' => 'date',
+            'appointment_time' => 'date',
+            'status' => AppointmentStatusEnum::class,
+        ];
+    }
+}
