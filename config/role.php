da9dbffa78bb3406895264b8feee2a6531ed036d
joerenzcarlmiranda <mirandajoerenz23@gmail.com>
2026-07-06 23:26:11 +0800

    feat: #51 Add Policy PatientPolicy

--- config/role.php
+++ config/role.php
@@ -1,12 +1,23 @@
 <?php
 
 return [
-    'admin' => ['*'],
-    'doctor' => [
+    'permissions' => [
+
+        'Admin' => [
+            'patient' => ['viewAny', 'view', 'create', 'update', 'delete'],
+        ],
+
+        'Doctor' => [
+            'patient' => ['viewAny', 'view', 'update'],
+        ],
+
+        'Receptionist' => [
+            'patient' => ['viewAny', 'view', 'create', 'update'],
+        ],
+
+        'Patient' => [
+            //
+        ],
 
     ],
-    'receptionist' => [
-
-    ],
-
 ];
