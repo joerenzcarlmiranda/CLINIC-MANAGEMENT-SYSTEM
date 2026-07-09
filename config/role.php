edd6a59b3d081b50ec2f7334d085127a2eb3c19e
joerenzcarlmiranda <mirandajoerenz23@gmail.com>
2026-07-09 21:56:34 +0800

    feat: #52 Add Policy DoctorPolicy

--- config/role.php
+++ config/role.php
@@ -5,14 +5,17 @@ return [
 
         'Admin' => [
             'patient' => ['viewAny', 'view', 'create', 'update', 'delete'],
+            'doctor'  => ['viewAny', 'view', 'create', 'update', 'delete'],
         ],
 
         'Doctor' => [
             'patient' => ['viewAny', 'view', 'update'],
+            'doctor'  => ['viewAny', 'view'],
         ],
 
         'Receptionist' => [
             'patient' => ['viewAny', 'view', 'create', 'update'],
+            'doctor'  => ['viewAny', 'view'],
         ],
 
         'Patient' => [
