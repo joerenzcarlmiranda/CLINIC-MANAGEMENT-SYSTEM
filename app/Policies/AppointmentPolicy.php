87bcc61b7033647889cccb1d473aa5e6030de399
Joerenz Carl M. Miranda <mirandajoerenz23@gmail.com>
2026-07-11 18:47:07 +0800

    feat: #53 Add Policy AppointmentPolicy (#62)

---
+++ app/Policies/AppointmentPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class AppointmentPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->can('doctor.viewAny');
+    }
+
+    public function view(User $user): bool
+    {
+        return $user->can('doctor.view');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->can('doctor.create');
+    }
+
+    public function update(User $user): bool
+    {
+        return $user->can('doctor.update');
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->can('doctor.delete');
+    }
+}
